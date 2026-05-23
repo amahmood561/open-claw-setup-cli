f9bca08ff6f2d16dcea72bbe085f66e7451068c9
Ali Mahmood <amahmood561@gmail.com>
2026-05-23 19:43:49 -0400

    update

--- OpenClawSetupApp/setupRunner.js
+++ OpenClawSetupApp/setupRunner.js
@@ -1,10 +1,20 @@
 const { execFile } = require('child_process');
+const fs = require('fs');
 const path = require('path');
 
 const DEFAULT_SETUP_SCRIPT = path.join(__dirname, '..', 'install-openclaw-stupid-simple-mac.sh');
+const BUNDLED_SETUP_SCRIPT = path.join(__dirname, 'install-openclaw-stupid-simple-mac.sh');
 
 function getSetupScriptPath() {
-  return process.env.OPENCLAW_SETUP_SCRIPT || DEFAULT_SETUP_SCRIPT;
+  if (process.env.OPENCLAW_SETUP_SCRIPT) {
+    return process.env.OPENCLAW_SETUP_SCRIPT;
+  }
+
+  if (fs.existsSync(DEFAULT_SETUP_SCRIPT)) {
+    return DEFAULT_SETUP_SCRIPT;
+  }
+
+  return BUNDLED_SETUP_SCRIPT;
 }
 
 function runSetupScript(scriptPath = getSetupScriptPath()) {
@@ -20,6 +30,7 @@ function runSetupScript(scriptPath = getSetupScriptPath()) {
 }
 
 module.exports = {
+  BUNDLED_SETUP_SCRIPT,
   DEFAULT_SETUP_SCRIPT,
   getSetupScriptPath,
   runSetupScript
